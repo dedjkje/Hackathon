f15520b767eec609619419c93582969d5797fa69
Антон <129394624+dedjkje@users.noreply.github.com>
2024-11-29 18:17:51 +0300

    1312321

--- Android/Assets/BlueBashyqa.cs
+++ Android/Assets/BlueBashyqa.cs
@@ -8,10 +8,12 @@ public class BlueBashyqa : MonoBehaviour
     public float health = 100;
     public Image hp;
     Vector2 width;
+    public float maxhp;
+    public Image damage;
     // Start is called before the first frame update
     void Start()
     {
-
+        maxhp = health;
     }
 
     // Update is called once per frame
@@ -27,9 +29,32 @@ public class BlueBashyqa : MonoBehaviour
         {
             health -= collision.gameObject.GetComponent<Damage>().damage;
             Debug.Log(collision.gameObject.GetComponent<Damage>().damage);
-            hp.rectTransform.localScale = new Vector2(health / 100, hp.rectTransform.localScale.y);
+            hp.rectTransform.localScale = new Vector2(health / maxhp, hp.rectTransform.localScale.y);
+            StartCoroutine(DecreaseHealthBar());
             GameObject.Find("Player 2(Clone)").GetComponent<Coins>().coins += collision.gameObject.GetComponent<Damage>().damage;
         }
 
+    }
+    private IEnumerator DecreaseHealthBar()
+    {
+        // ∆дем 0.5 секунды
+        yield return new WaitForSeconds(0.5f);
+
+        // ѕлавное уменьшение размера в течение 0.5 секунд
+        float targetScaleX = health / maxhp;
+        float currentScaleX = hp.rectTransform.localScale.x;
+        float duration = 0.5f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float newScaleX = Mathf.Lerp(currentScaleX, targetScaleX, elapsedTime / duration);
+            damage.rectTransform.localScale = new Vector2(newScaleX, hp.rectTransform.localScale.y);
+            yield return null; // ∆дем следующий кадр
+        }
+
+        // ”бедимс€, что мы установили окончательное значение
+        damage.rectTransform.localScale = new Vector2(targetScaleX, hp.rectTransform.localScale.y);
     }
 }
--- Android/Assets/RedBasnyaLeft.cs
+++ Android/Assets/RedBasnyaLeft.cs
@@ -7,11 +7,13 @@ public class RedBasnyaLeft : MonoBehaviour
 { 
     public float health = 100;
     public Image hp;
+    public Image damage;
     Vector2 width;
+    public float maxhp;
     // Start is called before the first frame update
     void Start()
     {
-        
+        maxhp = health;
     }
 
     // Update is called once per frame
@@ -27,11 +29,33 @@ public class RedBasnyaLeft : MonoBehaviour
         {
             health -= collision.gameObject.GetComponent<Damage>().damage;
             Debug.Log(collision.gameObject.GetComponent<Damage>().damage);
-            hp.rectTransform.localScale = new Vector2(health / 100, hp.rectTransform.localScale.y);
+            hp.rectTransform.localScale = new Vector2(health / maxhp, hp.rectTransform.localScale.y);
+            StartCoroutine(DecreaseHealthBar());
             GameObject.Find("Player 1(Clone)").GetComponent<Coins>().coins += collision.gameObject.GetComponent<Damage>().damage;
         }
         
     }
 
-    
+    private IEnumerator DecreaseHealthBar()
+    {
+        // Ждем 0.5 секунды
+        yield return new WaitForSeconds(0.5f);
+
+        // Плавное уменьшение размера в течение 0.5 секунд
+        float targetScaleX = health / maxhp;
+        float currentScaleX = hp.rectTransform.localScale.x;
+        float duration = 0.5f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += 0.05f;
+            float newScaleX = Mathf.Lerp(currentScaleX, targetScaleX, elapsedTime / duration);
+            damage.rectTransform.localScale = new Vector2(newScaleX, hp.rectTransform.localScale.y);
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        // Убедимся, что мы установили окончательное значение
+        damage.rectTransform.localScale = new Vector2(targetScaleX, hp.rectTransform.localScale.y);
+    }
 }
